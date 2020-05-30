23633b7fc916afa39bada9da7e21cbebb299e6a3
Allison Snipes <ajsnipes@usfca.edu>
2020-05-30 14:55:52 -0700

    first commit

---
+++ src/MessageDemo.java
@@ -0,0 +1,11 @@
+
+public class MessageDemo {
+
+	public static void main(String[] args) {
+		System.out.println("\t" + " J " + "\t" + "   A  " + "\t" + "  V  " + "\t" + "  V  " + "\t" + "  A  ");
+		System.out.println("\t" + " J " + "\t" + " A   A " + "\t" + "   V  " + "\t" + " V " + "\t" + " A  A");
+		System.out.println("\t" + "J" + "\t" + "\t" + "J" + "\t" + "V" + "\t" + "V" + "\t" + "A");
+		System.out.println("J" + "\t" + "J" + "\t" + "A" + "\t" + "V" + "\t" + "V" + "\t" + "A");
+	}
+
+}
