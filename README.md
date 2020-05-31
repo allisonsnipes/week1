b42521e4b5c54d7c02d0618481bfb7bf1570c908
Allison Snipes <ajsnipes@usfca.edu>
2020-05-30 19:03:17 -0700

    almost done

---
+++ README.md
@@ -0,0 +1,27 @@
+# Homework 1
+
+Using the picture provided provide a similar text file to display to the console a text image.
+
+## Getting Started
+
+Read the material provided, look at the video lectures, .
+
+### Prerequisities
+
+No prerequisities are needed to play the game. The user is to simply use to left and right arrow keys to bounce the ball around the screen to hit the blocks.
+
+## Built With
+
+This application was developed using Java, no other programs are needed to execute the program.
+
+## Authors
+
+Allison Juliette Snipes
+
+## License
+
+None used or needed.
+
+## Acknowlegements
+
+Dr. Sidney Ruby for teaching the summer seminar.
--- src/MessageDemo.java
+++ src/MessageDemo.java
@@ -1,10 +1,15 @@
+/* * * * * * * * * * * * * * * * * 
+* Allison Juliette Snipes        *
+* Created May 30, 2020           *
+* Homework Week 1                *
+* * * * * * * * * * * * * *  *   */
 
 public class MessageDemo {
 
 	public static void main(String[] args) {
 		System.out.println("\t" + " J " + "\t" + "   A  " + "\t" + "  V  " + "\t" + "  V  " + "\t" + "  A  ");
 		System.out.println("\t" + " J " + "\t" + " A   A " + "\t" + "   V  " + "\t" + " V " + "\t" + " A  A");
-		System.out.println("\t" + "J" + "\t" + "\t" + "J" + "\t" + "V" + "\t" + "V" + "\t" + "A");
+		System.out.println("\t" + " J " + "\t" + "\t" + "J" + "\t" + "V" + "\t" + "V" + "\t" + "A");
 		System.out.println("J" + "\t" + "J" + "\t" + "A" + "\t" + "V" + "\t" + "V" + "\t" + "A");
 	}
 
